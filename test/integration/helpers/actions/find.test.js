0d96d2ac5f4af3f1bc4e751c64409c46d7022479
Jorge Vargas <jorgevargasla@gmail.com>
2021-01-26 13:11:31 -0500

    fix: sync parse query remove await

--- test/integration/helpers/actions/find.test.js
+++ test/integration/helpers/actions/find.test.js
@@ -1,6 +1,6 @@
 describe('Find Action', function () {
   it('Should return ["list", "count"] array', async function () {
-    const filter = await sails.helpers.parse.query('user', 'find');
+    const filter = sails.helpers.parse.query('user', 'find');
 
     const res = await sails.helpers.actions.find(filter);
 
@@ -8,7 +8,7 @@ describe('Find Action', function () {
   });
 
   it('Should return five (5) items in list', async function () {
-    const filter = await sails.helpers.parse.query('user', 'find', {
+    const filter = sails.helpers.parse.query('user', 'find', {
       perPage: 5,
     });
 
